4761b90dd042443da884ea7784326bb192991f56
Nooran Tarek <102181986+NooranTarek@users.noreply.github.com>
2024-05-17 15:27:20 +0300

    Update db.js

--- graphql/db.js
+++ graphql/db.js
@@ -2,7 +2,6 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
   host: 'localhost',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
